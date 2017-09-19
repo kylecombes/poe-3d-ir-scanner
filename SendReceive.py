6ae0cf4ff1a5cf491d730103e971a3ffe71569d1
Kyle Combes <kylecombes@gmail.com>
2017-09-19 09:17:05 -0400

    Sending data to computer and parsing working

--- SendReceive.py
+++ SendReceive.py
@@ -1,6 +1,5 @@
 #!/usr/local/bin/python
 
-import re
 from serial import Serial, SerialException
 import time
 
@@ -38,16 +37,3 @@ while True:
     if radius:
         print('Radius: {}\tAngle: {}'.format(radius, theta))
     time.sleep(0.3)
-
-    # try:
-    #     cmd_id = int(input("Please enter a command ID (1 - read potentiometer, 2 - read the button: "))
-    #     if int(cmd_id) > 2 or int(cmd_id) < 1:
-    #         print("Values other than 1 or 2 are ignored.")
-    #     else:
-    #         cxn.write([int(cmd_id)])
-    #         while cxn.inWaiting() < 1:
-    #             pass
-    #         result = cxn.readline();
-    #         print(result)
-    # except ValueError:
-    #     print("You must enter an integer value between 1 and 2.")
--- lab2.ino
+++ lab2.ino
@@ -1,3 +1,7 @@
+#include <Servo.h>
+
+Servo servoPan;  // create servo object to control a servo
+
 /*
 
 I intentionally use the const byte construct here
@@ -5,15 +9,20 @@ instead of #define. It's less dangerous (no name collision possible)
 and safer since variables have scope.
 */
 const byte PUSH_BUTTON = 8;
-const byte POT = A0;
+const byte IR_SENSOR = A0;
 const byte CMD_READ_POT = 1;
 const byte CMD_READ_BTN = 2;
+const byte XY_SERVO = 3;
+
+int THETA_MIN = 0;
+int THETA_MAX = 180;
 
 long prev_t = 0;
 int radius = 100;
 int pot_value = 100;
-int theta = 0;
-byte cmd_id = 0; 
+int theta = 90;
+int thetaStep = 0;
+byte cmd_id = 0;
 
 byte btn_state = LOW;
 
@@ -22,34 +31,61 @@ String result="";
 void setup() {
   //Setup input and outputs: LEDs out, pushbutton in.
   pinMode(PUSH_BUTTON, INPUT);
+  servoPan.attach(XY_SERVO);
   Serial.begin(9600);
 }
 
+// Move the panning servo to a particular angle
+void setTheta(int theta) {
+  if (theta > THETA_MAX) {
+    theta = THETA_MAX;
+  } else if (theta < THETA_MIN) {
+    theta = THETA_MIN;
+  }
+  servoPan.write(theta);
+}
+
+// Take the average distance reading over 10 readings
+int READINGS_PER_ANGLE = 20;
+int distanceSum = 0;
+int readCount = 0;
+
 void loop() {
-//  byte btn_state = digitalRead(PUSH_BUTTON);
-  radius = analogRead(POT);
 
-  transmitData(radius, theta);
+  // Take a reading, add it to our running sum, and increment our read counter
+  distanceSum += readDistFromSensor();
+  readCount++;
 
-//  if(Serial.available() > 0) {
-//    cmd_id = Serial.read();
-//  } else {
-//    cmd_id = 0;
-//  }
+  if (readCount == READINGS_PER_ANGLE) {
 
-//  switch(cmd_id){
-//    case CMD_READ_POT:
-//      result = result + "Potentiometer reads: " + pot_value;
-//      Serial.println(sensor_value);
-//      result = "";
-//      break;
-//    case CMD_READ_BTN:
-//      result = result + "Button state: " + btn_state;
-//      Serial.println(result);
-//      result = "";
-//      break;
-//    break;
-//  }
+    // Calculate the average distance reading
+    int distance = distanceSum / READINGS_PER_ANGLE;
+    
+    // Send our position and distance reading to the computer
+    transmitData(distance, theta);
+
+    // Pan to the next position
+    theta += thetaStep;
+    setTheta(theta);
+    
+    // Change the direction for the next theta change, if we've reached the max or min
+    if (theta > THETA_MAX || theta < THETA_MIN) {
+      thetaStep = -thetaStep;
+    }
+
+    // Reset counter and sum
+    distanceSum = 0;
+    readCount = 0;
+  }
+  // Wait 10ms between readings
+  delay(10);
+  
+}
+
+// Get the distance (in inches) measured by the IR sensor
+float readDistFromSensor() {
+  // Slope and intercept determined by calibration experiment
+  return -19.3 * ((float)analogRead(IR_SENSOR)) + 245;
 }
 
 void transmitData(int radius, int theta) {
